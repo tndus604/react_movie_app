c417d44b6507a2a0feba8f735f9287db76a6ee77
SuYoun <tndus0604@hotmail.com>
2020-11-19 19:46:19 -0500

    axios to get movie url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import axios from "axios";
 
-function App() {
-  return <div>Hello!!!</div>;
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies = []
+  }
+
+  async getMovies() {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+  
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        {this.state.isLoading ? "Loading..." : "We are READY"}
+      </div>
+    );
+  }
 }
+
 
 export default App;
