13f903a20069aade0cec0c458a40e9808a83cb96
SuYoun <tndus0604@hotmail.com>
2020-11-20 16:51:03 -0500

    rendering movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
   state = {
     isLoading: true,
-    movies = []
+    movies: []
   }
 
-  async getMovies() {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
-  }
-
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
   componentDidMount() {
     this.getMovies();
   }
-  
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        {this.state.isLoading ? "Loading..." : "We are READY"}
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+          )
+        )}
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie({id, year, title, summary, poster}) {
+    return(
+    <div>
+        <img src={poster} alt={title} />
+        <h4>{title}</h4>
+        <p>{year}</p>
+        <p>{summary}</p>
+        <p>{id}</p>
+    </div>
+    )
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+export default Movie;
